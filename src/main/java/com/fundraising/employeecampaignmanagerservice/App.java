bab634b4393fbc7807fb4a3991a1588c5f5a6e66
EXC46189 <julian.markowskyj@claro.com.ar>
2020-04-21 21:47:09 -0300

    Initial commit

---
+++ src/main/java/com/fundraising/employeecampaignmanagerservice/App.java
@@ -0,0 +1,13 @@
+package com.fundraising.employeecampaignmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
